4d793f8c91436326206b0bad9031b054f31f3902
Coron4444 <seimu7x@gmail.com>
2018-05-07 14:21:21 +0900

    ノブナリがっちゃんこ成功

--- Assets/GameScene/nobunari/Koudai3D_Move.cs
+++ Assets/GameScene/nobunari/Koudai3D_Move.cs
@@ -6,16 +6,22 @@ public class Koudai3D_Move : MonoBehaviour {
     private float fPosX = 0;　//X軸移動量
     private float fPosY = 0;  //Y軸移動量
     private float fPosZ = 0;  //Z軸移動量
+    private float fRotationY = 0;
     private float fLimit_y = -20.0f;
     private bool bLockOn = false; //ロックオン判定
-    private bool bhorizon = false;//x軸移動方向判定
-    private bool bfront = false;  //z軸移動方向判定
+    private bool bRotation = false;
+    private int bhorizon = 0;//x軸移動方向判定   1=PLUS  2=MINUS  0=NULL
+    private int bfront = 0;  //z軸移動方向判定   1=PLUS  2=MINUS  0=NULL
     private int nCnt = 0;
     public GameObject ball;
 
     // Use this for initialization
     void Start ()
     {
+        bhorizon = 2;
+        bfront = 0;
+        //int rand;
+        //rand = Random.Range();
     }
     
 	void Update ()
@@ -27,20 +33,26 @@ public class Koudai3D_Move : MonoBehaviour {
         {
 		    switch (bhorizon)
 		    {
-			    case false:
-				    fPosX = Time.deltaTime * -10.0f;
+                case 0:
+                    fPosX = 0;
+                    break;
+			    case 1:
+				    fPosX = Time.deltaTime * 5.0f;
 				    break;
-			    case true:
-
-				    break;
+			    case 2:
+                    fPosX = Time.deltaTime * -5.0f;
+                    break;
 		    }
 		    switch (bfront)
 		    {
-			    case false:
-				    fPosZ = Time.deltaTime * 10.0f;
+                case 0:
+                    fPosZ = 0;
+                    break;
+                case 1:
+				    fPosZ = Time.deltaTime * 5.0f;
 				    break;
-			    case true:
-				    fPosZ = Time.deltaTime * -10.0f;
+			    case 2:
+				    fPosZ = Time.deltaTime * -5.0f;
 				    break;
 		    }
 		    transform.Translate(fPosX, fPosY, fPosZ);
@@ -84,31 +96,7 @@ public class Koudai3D_Move : MonoBehaviour {
     void OnCollisionEnter (Collision other)
 	{
 		//------------------------------------------------------------------------------
-		if (other.gameObject.name == "kabe2")
-		{
-			switch (bhorizon)
-			{
-				case false:
-                    bhorizon = true;
-					fPosX = 0.0f;
-					break;
-			}
-		}
-        if (other.gameObject.name == "kabe3")
-        {
-            switch (bhorizon)
-            {
-                case false:
-                    bfront = true;
-                    fPosX = 0.0f;
-                    break;
-            }
-        }
-        if (other.gameObject.name == "goal")
-		{
-			Destroy(gameObject);
-			Application.Quit();
-		}
+
 	}
 	// 衝突したオブジェクトが離れる瞬間を検出
 	void OnCollisionExit (Collision other)
@@ -119,14 +107,6 @@ public class Koudai3D_Move : MonoBehaviour {
 	// 衝突中を検出
 	void OnCollisionStay (Collision other)
 	{
-		if (other.gameObject.name == "wall4" || other.gameObject.name == "wall3")
-		{
-			bhorizon = false;
-		}
-		if (other.gameObject.name == "wall")
-		{
-			bfront = false;
-		}
 
         if(bLockOn==true)
         { 
@@ -148,12 +128,180 @@ public class Koudai3D_Move : MonoBehaviour {
     //-----------------------------------------------------------------------------------------
     //トリガーに接触した瞬間
     void OnTriggerEnter(Collider other)
-    {if(bLockOn==false)
+    {
+        if (bLockOn==false)
         { 
             if (other.gameObject.tag=="ball")
             {
                 ball = other.gameObject;
                 bLockOn = true;
+            }
+        }
+
+        if(other.gameObject.name=="judge")
+        {
+            //transform.Rotate(0,90,0);
+            int rand;
+            rand = Random.Range(0,2+1); //int : 第二引数は含まない->+1で調整
+            if (rand == 0)
+            {
+                bfront = 0;
+               bhorizon = 1;
+            }
+            if (rand == 1)
+            {
+                bfront = 0;
+                bhorizon = 2;
+            }
+            if (rand == 2)
+            {
+                bfront = 1;
+                bhorizon = 0;
+            }
+        }
+        if (other.gameObject.name == "judge2")
+        {
+            int rand;
+            rand = Random.Range(0, 1+1);
+            if (rand == 0)
+            {
+                bfront = 0;
+                bhorizon = 1;
+            }
+            if (rand == 1)
+            {
+                bfront = 1;
+                bhorizon = 0;
+            }
+        }
+        if (other.gameObject.name == "judge3")
+        {
+            int rand;
+            rand = Random.Range(0, 1+1);
+            if (rand == 0)
+            {
+                bfront = 0;
+                bhorizon = 2;
+            }
+            if (rand == 1)
+            {
+                bfront = 1;
+                bhorizon = 0;
+            }
+        }
+        if (other.gameObject.name == "judge4")
+        {
+            int rand;
+            rand = Random.Range(0, 2 + 1);
+            if (rand == 0)
+            {
+                bfront = 0;
+                bhorizon = 1;
+            }
+            if (rand == 1)
+            {
+                bfront = 1;
+                bhorizon = 0;
+            }
+            if (rand == 2)
+            {
+                bfront = 2;
+                bhorizon = 0;
+            }
+        }
+        if (other.gameObject.name == "judge5")
+        {
+            int rand;
+            rand = Random.Range(0, 1 + 1);
+            if (rand == 0)
+            {
+                bfront = 2;
+                bhorizon = 0;
+            }
+            if (rand == 1)
+            {
+                bfront = 0;
+                bhorizon = 1;
+            }
+        }
+        if (other.gameObject.name == "judge6")
+        {
+            int rand;
+            rand = Random.Range(0, 2 + 1);
+            if (rand == 0)
+            {
+                bfront = 0;
+                bhorizon = 2;
+            }
+            if (rand == 1)
+            {
+                bfront = 1;
+                bhorizon = 0;
+            }
+            if (rand == 2)
+            {
+                bfront = 2;
+                bhorizon = 0;
+            }
+        }
+        if (other.gameObject.name == "judge7")
+        {
+            int rand;
+            rand = Random.Range(0, 1 + 1);
+            if (rand == 0)
+            {
+                bfront = 2;
+                bhorizon = 0;
+            }
+            if (rand == 1)
+            {
+                bfront = 0;
+                bhorizon = 2;
+            }
+        }
+        if (other.gameObject.name == "judge8")
+        {
+            int rand;
+            rand = Random.Range(0, 3 + 1);
+            if (rand == 0)
+            {
+                bfront = 2;
+                bhorizon = 0;
+            }
+            if (rand == 1)
+            {
+                bfront = 1;
+                bhorizon = 0;
+            }
+            if (rand == 2)
+            {
+                bfront = 0;
+                bhorizon = 1;
+            }
+            if (rand == 3)
+            {
+                bfront = 0;
+                bhorizon = 2;
+            }
+        }
+        if (other.gameObject.name == "judge9")
+        {
+            int rand;
+            rand = Random.Range(0, 2 + 1);
+            if (rand == 0)
+            {
+                bfront = 0;
+                bhorizon = 1;
+            }
+            if (rand == 1)
+            {
+                bfront = 0;
+                bhorizon = 2;
+            }
+            if (rand == 2)
+            {
+                bfront = 2;
+                bhorizon = 0;
             }
         }
     }
